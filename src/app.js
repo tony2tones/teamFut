cf63b2901a1ea5eecbe1cf6aef5ac148deee465b
Anthony <anthonymarques@outlook.com>
2018-11-25 15:24:30 +0200

    added react-router-dom and other component

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+
 const App = () => {
   return (
-    <div>
-      <p>React here!</p>
-    </div>
+    <BrowserRouter>
+      <div>
+        <p>React here!</p>
+      </div>
+    </BrowserRouter>
   );
 };
 export default App;
