37da4134a82ef051909669ae001dfef40ace38e6
Anthony <anthonymarques@outlook.com>
2018-11-26 21:03:14 +0200

    SignedInOut links and adding it to the nav bar

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import SignedInLinks from "./SignedInlinks";
+import SignedOutLinks from "./SignedOutlinks";
 
 const NavBar = () => {
   return (
     <nav className="nav-wrapper grey darken-3">
       <div className="container">
         <Link to="/" className="brand-logo">TeamFut</Link>
+        <SignedInLinks />
+        <SignedOutLinks />
       </div>
 
     </nav>
---
+++ src/components/layout/SignedInlinks.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const SignedInLinks = () => {
+  return (
+    <ul className="right">
+        <li><NavLink to="/">New Event</NavLink></li>
+        <li><NavLink to="/">Log out</NavLink></li>
+        <li><NavLink to="/" className="btn btn-floating blue lighten-1">TF</NavLink></li>
+    </ul>
+  );
+};
+
+export default SignedInLinks;
