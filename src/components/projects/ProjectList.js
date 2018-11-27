02418a892fd8dac7d3875f76bedf0dcdbc952cac
Anthony Marques <anthony@armorica.gk>
2018-11-27 15:28:27 +0200

    Added ProjectList to Dashboard

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -2,7 +2,33 @@ import React from 'react';
 
 const ProjectList = () =>{
     return (
+        <div className="project-list section">
         
+        <div className="card z-depth-0 project-summary">
+            <div className="card-content grey-text text-darken-3">
+            <span className="card-title">Project Title</span>
+            <p>Posted by Tony</p>
+            <p className="grey-text">3 September 2amzies</p>
+            </div>
+        </div>
+
+        <div className="card z-depth-0 project-summary">
+            <div className="card-content grey-text text-darken-3">
+            <span className="card-title">Project Title</span>
+            <p>Posted by Tony</p>
+            <p className="grey-text">3 September 2amzies</p>
+            </div>
+        </div>
+
+        <div className="card z-depth-0 project-summary">
+            <div className="card-content grey-text text-darken-3">
+            <span className="card-title">Project Title</span>
+            <p>Posted by Tony</p>
+            <p className="grey-text">3 September 2amzies</p>
+            </div>
+        </div>
+
+        </div>
     )
 }
 
