94205ab4cff6c3402ed8ff5552bc39ab76d11a43
Anthony <anthonymarques@outlook.com>
2018-11-28 10:48:37 +0200

    added some content

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Navbar from './components/layout/Navbar';
 import Dashboard from './components/dashboard/Dashboard';
+import ProjectDetails from './components/projects/ProjectDetails';
 
 const App = () => {
   return (
@@ -10,7 +11,8 @@ const App = () => {
       <div className="App">
         <Navbar />
         <Switch>
-            <Route path="/" component={Dashboard} />
+            <Route exact path="/" component={Dashboard} />
+            <Route path="/project/:id" component={ProjectDetails} />
         </Switch>
       </div>
     </BrowserRouter>
---
+++ src/components/projects/ProjectDetails.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const ProjectDetails = () => {
+  return (
+    <div className="container section project-details">
+      <div className="card-content">
+        <div className="card-title">Project Title</div>
+        <p> toasty toasty taotste</p>
+      </div>
+      <div className="card-action grey lighten-4 grey text">
+        <div> Posted by Tony</div>
+        <div>2nd September 2am</div>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectDetails;
