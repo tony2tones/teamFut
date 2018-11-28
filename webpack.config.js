513ae412e2cbc548f3f50012ed01f20c256cfa24
Anthony <anthonymarques@outlook.com>
2018-11-28 14:04:36 +0200

    trying to figure out why the main.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,12 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 module.exports = {
+  entry: [
+    './src/index.js'
+],
+output: {
+    path: __dirname,
+    filename: 'app/js/main.js'
+},
   module: {
     rules: [
       {
