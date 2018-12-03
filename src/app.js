c84c93fc1d97de2cdf12a5d5f8c469b24b870757
Anthony Marques <anthony@armorica.gk>
2018-12-03 08:42:01 +0200

    adding createProject flow

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ import Dashboard from './components/dashboard/Dashboard';
 import ProjectDetails from './components/projects/ProjectDetails';
 import SignIn from './components/auth/SignIn';
 import SignUp from './components/auth/SignUp';
+import CreateProject from './components/projects/CreateProject'
 
 const App = () => {
   return (
@@ -17,6 +18,7 @@ const App = () => {
             <Route path="/project/:id" component={ProjectDetails} />
             <Route path='/signin' component={SignIn} />
             <Route path='/signup' component={SignUp} />
+            <Route path='/create' component={CreateProject} />
         </Switch>
       </div>
     </BrowserRouter>
