ed691ba5dcae7e4937ba17a0a236bf59256934e0
Anthony Marques <anthony@armorica.gk>
2018-12-03 08:20:40 +0200

    adding username instead of first name and last name

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ import Navbar from './components/layout/Navbar';
 import Dashboard from './components/dashboard/Dashboard';
 import ProjectDetails from './components/projects/ProjectDetails';
 import SignIn from './components/auth/SignIn';
+import SignUp from './components/auth/SignUp';
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
             <Route path='/' exact component={Dashboard} />
             <Route path="/project/:id" component={ProjectDetails} />
             <Route path='/signin' component={SignIn} />
+            <Route path='/signup' component={SignUp} />
         </Switch>
       </div>
     </BrowserRouter>
