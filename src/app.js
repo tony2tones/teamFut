78d35c06ed4aac36044da53c27eacd93a75fd4e6
Anthony Marques <anthony@armorica.gk>
2018-12-10 11:12:32 +0200

    added React-redux and redux and setup reducers

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ import ProjectDetails from './components/projects/ProjectDetails';
 import SignIn from './components/auth/SignIn';
 import SignUp from './components/auth/SignUp';
 import CreateProject from './components/projects/CreateProject'
+import './index';
 
 const App = () => {
   return (
--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './app';
+import registerServiceWorker from './registerServiceWorker';
+import { createStore } from 'redux';
+import rootReducer from './store/reducers/rootReducer';
+
+const store = createStore(rootReducer);
+
+ReactDOM.render(<App />, document.getElementById('root'));
+)
+registerServiceWorker();
