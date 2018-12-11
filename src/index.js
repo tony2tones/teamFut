6f800dbc6a30489d89a40f8999201087adf4e8c8
Anthony Marques <anthony@armorica.gk>
2018-12-11 15:38:24 +0200

    added firebase goodies and getting it set up

--- src/index.js
+++ src/index.js
@@ -6,7 +6,9 @@ import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './store/reducers/rootReducer';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
+import { getFirestore } from 'redux=firestore';
+import { getFirestore } from 'react-redux-firebase';
 
-const store = createStore(rootReducer,applyMiddleware(thunk));
+const store = createStore(rootReducer,applyMiddleware(thunk.withExtraArgument({getFirebase, getFirestore})));
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('app'));
