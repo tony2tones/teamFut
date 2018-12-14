fc29aeec04e32364267492048006487b263a0c29
Anthony Marques <anthony@armorica.gk>
2018-12-14 12:59:21 +0200

    added redux and react firebase and firestore

--- src/index.js
+++ src/index.js
@@ -2,13 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './app';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from './store/reducers/rootReducer';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
-import { getFirestore } from 'redux-firestore';
-import { getFirebase } from 'react-redux-firebase';
+import { reduxFirestore, getFirestore } from 'redux-firestore';
+import { reactReduxFirebase, getFirebase } from 'react-redux-firebase';
 
-const store = createStore(rootReducer,applyMiddleware(thunk.withExtraArgument({getFirebase, getFirestore})));
+const store = createStore(rootReducer,
+    compose(
+        applyMiddleware(thunk.withExtraArgument({getFirebase, getFirestore}))
+        )
+);
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('app'));
