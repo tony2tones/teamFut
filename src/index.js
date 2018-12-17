2f388121bb0af46fd8c2de8b6086982775d5c058
Anthony <anthonymarques@outlook.com>
2018-12-17 12:06:44 +0200

    fixed an issue with my import and working on projectActions

--- src/index.js
+++ src/index.js
@@ -8,10 +8,13 @@ import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import { reduxFirestore, getFirestore } from 'redux-firestore';
 import { reactReduxFirebase, getFirebase } from 'react-redux-firebase';
+import fbConfig from './config/fbConfig'
 
 const store = createStore(rootReducer,
     compose(
-        applyMiddleware(thunk.withExtraArgument({getFirebase, getFirestore}))
+        applyMiddleware(thunk.withExtraArgument({getFirebase, getFirestore})),
+        reduxFirestore(fbConfig),
+        reactReduxFirebase(fbConfig),
         )
 );
 
