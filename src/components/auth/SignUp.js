b7408a4222237a602644207e6302c7718e887908
Anthony <anthonymarques@outlook.com>
2018-12-27 14:42:43 +0200

    fixed Signup flow

--- src/components/auth/SignUp.js
+++ src/components/auth/SignUp.js
@@ -1,69 +1,73 @@
-import React, { Component } from 'react';
-import { Redirect } from 'react-router-dom';
-import { connect } from 'react-redux';
-import { signUp } from '../../store/actions/authActions';
+import React, { Component } from "react";
+import { Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+import { signUp } from "../../store/actions/authActions";
 
 class SignUp extends Component {
-    state = {
-        email: '',
-        password: '',
-        firstName: '',
-        lastName: ''
-    }
+  state = {
+    email: "",
+    password: "",
+    firstName: "",
+    lastName: ""
+  };
 
-    handleChange = (e) => {
-        this.setState({
-            [e.target.id]: e.target.value
-        })
-    }
+  handleChange = e => {
+    this.setState({
+      [e.target.id]: e.target.value
+    });
+  };
 
-    handleSubmit = (e) => {
-        e.preventDefault();
-        this.props.signUn(this.state)
-    }
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.signUp(this.state);
+  };
 
-    render() {
-        const { auth }= this.props;
-        if(auth.uid) return <Redirect to='/' />
-        return (
-            <div className="container">
-                <form onSubmit={this.handleSubmit} className="white">
-                    <h5 className="grey-text text-darken-3">Sign Up</h5>
-                    <div className="input-field">
-                        <label htmlFor="email">Email</label>
-                        <input type="email" id="email" onChange={this.handleChange} />
-                    </div>
-                    <div className="input-field">
-                        <label htmlFor="password">Password</label>
-                        <input type="password" id="password" onChange={this.handleChange} />
-                    </div>
-                    <div className="input-field">
-                        <label htmlFor="firstName">First Name</label>
-                        <input type="text" id="firstName" onChange={this.handleChange} />
-                    </div>
-                    <div className="input-field">
-                        <label htmlFor="lastName">Last Name</label>
-                        <input type="text" id="lastName" onChange={this.handleChange} />
-                    </div>
-                    <div className="input-field">
-                        <button className="btn blue lighten-1 z-depth-0">Submit</button>
-                    </div>
-                </form>
-            </div>
-        )
-    }
+  render() {
+    const { authError, auth } = this.props;
+    if (auth.uid) return <Redirect to="/" />;
+    return (
+      <div className="container">
+        <form onSubmit={this.handleSubmit} className="white">
+          <h5 className="grey-text text-darken-3">Sign Up</h5>
+          <div className="input-field">
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email" onChange={this.handleChange} />
+          </div>
+          <div className="input-field">
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" onChange={this.handleChange} />
+          </div>
+          <div className="input-field">
+            <label htmlFor="firstName">First Name</label>
+            <input type="text" id="firstName" onChange={this.handleChange} />
+          </div>
+          <div className="input-field">
+            <label htmlFor="lastName">Last Name</label>
+            <input type="text" id="lastName" onChange={this.handleChange} />
+          </div>
+          <div className="input-field">
+            <button className="btn blue lighten-1 z-depth-0">Submit</button>
+          </div>
+          <div className="red-text">
+            {/* {authError ? <p> {authError} </p> : null} */}
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
 
-const mapStateToProps = (state) => {
-    return {
-        auth: state.firebase.auth
-    }
-}
+const mapStateToProps = state => {
+  return {
+    authError: state.auth.authError,
+    auth: state.firebase.auth
+  };
+};
 
-const mapDispatchToProps = (dispatch) => {
-    return {
-        signUp: (newUser) => dispatch(signUp(newUser))
-    }
-}
+const mapDispatchToProps = dispatch => {
+  return {
+    signUp: newUser => dispatch(signUp(newUser))
+  };
+};
 
-export default connect(mapDispatchToProps,mapStateToProps)(SignUp);
+export default connect(mapStateToProps, mapDispatchToProps)(SignUp);
