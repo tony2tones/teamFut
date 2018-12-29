af95a4d5130b066877f756cef4dbfa19128ba37f
Anthony <anthonymarques@outlook.com>
2018-12-29 17:52:29 +0200

    working on date formatting

--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -1,13 +1,15 @@
 import React from "react";
 
 const ProjectSummary = ({project}) => {
+  console.log(project.createdAt.toDate().toString());
+
   return (
     <div className="project-list section">
       <div className="card z-depth-0 project-summary">
         <div className="card-content grey-text text-darken-3">
           <span className="card-title">{project.title} </span>
           <p>Posted by {project.authorFirstName} {project.authorLastName}</p>
-          <p className="grey-text">3 September 2amzies</p>
+          <p className="grey-text"> {project.createdAt.toDate().toString()} </p>
         </div>
       </div>
     </div>
