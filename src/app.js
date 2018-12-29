f3011614db50bc6c76a49cadd1b3933d84c14572
Anthony <anthonymarques@outlook.com>
2018-12-29 16:46:44 +0200

    removed a console log and removed an import that is not being used

--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Navbar from './components/layout/Navbar';
 import Dashboard from './components/dashboard/Dashboard';
--- src/components/layout/SignedInlinks.js
+++ src/components/layout/SignedInlinks.js
@@ -4,7 +4,6 @@ import { connect } from 'react-redux';
 import { signOut } from '../../store/actions/authActions'
 
 const SignedInLinks = (props) => {
-  console.log(props);
   return (
     <ul className="right">
         <li><NavLink to="/create">New Event</NavLink></li>
