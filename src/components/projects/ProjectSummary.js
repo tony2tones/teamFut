7050a84358ba72c2d9cbda7301e360e2f596116c
Anthony <anthonymarques@outlook.com>
2018-12-29 15:11:56 +0200

    added author's details to created posts

--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -6,7 +6,7 @@ const ProjectSummary = ({project}) => {
       <div className="card z-depth-0 project-summary">
         <div className="card-content grey-text text-darken-3">
           <span className="card-title">{project.title} </span>
-          <p>Posted by Tony</p>
+          <p>Posted by {project.firstName} {project.lastName}</p>
           <p className="grey-text">3 September 2amzies</p>
         </div>
       </div>
