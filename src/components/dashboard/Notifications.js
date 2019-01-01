a5b56dbfea184ef70dfff943164cffc474672fef
Anthony <anthonymarques@outlook.com>
2019-01-01 23:03:20 +0200

    working on firebase functions

--- src/components/dashboard/Notifications.js
+++ src/components/dashboard/Notifications.js
@@ -1,9 +1,21 @@
 import React from "react"
 
-const Notification = () => {
+const Notification = (props) => {
     return(
-        <div>
-            <p>Notifications</p>
+        <div className="section">
+        <div className="ard z-depth-0">
+        <div className="card-content">
+        <span className="card-title"> Notifications</span>
+        <ul className="notification">
+            <li>notification</li>
+            <li>notification</li>
+            <li>notification</li>
+            <li>notification</li>
+            <li>notification</li>
+            <li>notification</li>
+        </ul>
+        </div>
+        </div>
         </div>
     )
 }
