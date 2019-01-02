57cd8beb5795ea2187bad8889218cb067163edfb
Anthony Marques <anthony@armorica.gk>
2019-01-02 08:59:56 +0200

    formatted notifications

--- src/components/dashboard/Notifications.js
+++ src/components/dashboard/Notifications.js
@@ -1,30 +1,31 @@
-import React from "react"
-import {format} from 'timeago.js';
+import React from "react";
+import { format } from "timeago.js";
 
-const Notification = (props) => {
-    const {notifications} = props;
-    return(
-        <div className="section">
-        <div className="ard z-depth-0">
+const Notification = props => {
+  const { notifications } = props;
+  return (
+    <div className="section">
+      <div className="ard z-depth-0">
         <div className="card-content">
-        <span className="card-title"> Notifications</span>
-        <ul className="notification">
-            {notifications && notifications.map(item => {
+          <span className="card-title"> Notifications</span>
+          <ul className="notification">
+            {notifications &&
+              notifications.map(item => {
                 return (
-                    <li key={item.id}>
+                  <li key={item.id}>
                     <span className="pink-text">{item.user} </span>
                     <span>{item.content}</span>
-                    <div className='grey-text note-date'>
-                    {format(item.time.toDate().toString())}
+                    <div className="grey-text note-date">
+                      {format(item.time.toDate().toString())}
                     </div>
-                    </li>
-                )
-            })}
-        </ul>
+                  </li>
+                );
+              })}
+          </ul>
         </div>
-        </div>
-        </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default Notification;
