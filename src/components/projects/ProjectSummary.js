7374637e3ea48140acffdca962f829294ebcaf52
Anthony <anthony.marque@gmail.com>
2019-01-03 15:35:22 +0200

    removing projectid

--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -12,7 +12,7 @@ const ProjectSummary = ({project}) => {
     <div className="mdc-card my-card">
       <div className="card z-depth-0 project-summary">
         <div className="card-content grey-text text-darken-3" style={cardStyle}>
-          <span className="mdc-card__media-content">{project.title} {project.id}</span>
+          <span className="mdc-card__media-content">{project.title} </span>
           <p>Posted by {project.authorFirstName} {project.authorLastName}</p>
           <p className="grey-text"> {format(project.createdAt.toDate().toString())} </p>
         </div>
